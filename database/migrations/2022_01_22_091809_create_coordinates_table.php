3691879e1fb3f3d145f1f0313ecd1397cd9209a2
Élie Meignan <contact@eliepse.fr>
2022-01-23 15:18:15 +0100

    Fix wrong argument Type

--- database/migrations/2022_01_22_091809_create_coordinates_table.php
+++ database/migrations/2022_01_22_091809_create_coordinates_table.php
@@ -3,8 +3,8 @@
 use App\Models\Trace;
 use Illuminate\Database\Grammar;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use MStaack\LaravelPostgis\Schema\Blueprint;
 
 class CreateCoordinatesTable extends Migration
 {
@@ -15,15 +15,11 @@ class CreateCoordinatesTable extends Migration
 	 */
 	public function up()
 	{
-		Grammar::macro("typeGeoPointZ", function () {
-			return "geography(POINTZ)";
-		});
 		Schema::createExtensionIfNotExists('postgis');
 		Schema::create('coordinates', function (Blueprint $table) {
 			$table->id();
 			$table->foreignIdFor(Trace::class);
 			$table->point("location");
-//			$table->addColumn("GeoPointZ", "location");
 			$table->float("precision")->nullable();
 			$table->timestamp("recorded_at");
 
