f49409f1345d53378247e9a6b3c4be1ff415dacc
Élie Meignan <contact@eliepse.fr>
2022-01-29 17:43:22 +0100

    Remove default settings to allow default Pusher ones

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,8 +36,8 @@ return [
 			'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
 			'app_id' => env('PUSHER_APP_ID', 'app-id'),
 			'options' => [
-				'host' => env('PUSHER_HOST', '127.0.0.1'),
-				'port' => env('PUSHER_PORT', 6001),
+				'host' => env('PUSHER_HOST'),
+				'port' => env('PUSHER_PORT'),
 				'scheme' => env('PUSHER_SCHEME', 'http'),
 				'cluster' => env("PUSHER_APP_CLUSTER"),
 				'encrypted' => false,
