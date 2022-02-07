e7896d95a387ba877fa2ae9062618ee0bb97a3b4
Élie Meignan <contact@eliepse.fr>
2022-02-07 22:34:20 +0100

    Fix CSP policy

--- app/Http/Csp/CustomPolicy.php
+++ app/Http/Csp/CustomPolicy.php
@@ -10,28 +10,33 @@ class CustomPolicy extends \Spatie\Csp\Policies\Basic
 	public function configure()
 	{
 		$this
+			// Part of default
 			->addDirective(Directive::BASE, Keyword::SELF)
 			->addDirective(Directive::CONNECT, Keyword::SELF)
 			->addDirective(Directive::DEFAULT, Keyword::SELF)
-			->addDirective(Directive::DEFAULT, "http://localhost:8080")
-			->addDirective(Directive::DEFAULT, "https://fonts.gstatic.com")
-			->addDirective(Directive::DEFAULT, "wss://*.pusher.com")
 			->addDirective(Directive::FORM_ACTION, Keyword::SELF)
 			->addDirective(Directive::IMG, Keyword::SELF)
-			->addDirective(Directive::IMG, "*.openstreetmap.fr")
+			->addDirective(Directive::MEDIA, Keyword::SELF)
 			->addDirective(Directive::OBJECT, Keyword::NONE)
 			->addDirective(Directive::SCRIPT, Keyword::SELF)
 			->addDirective(Directive::STYLE, Keyword::SELF)
-			->addDirective(Directive::STYLE, "http://localhost:8080")
-			->addDirective(Directive::STYLE, "https://fonts.googleapis.com")
-			->addDirective(Directive::STYLE, Keyword::UNSAFE_INLINE)
-			->addDirective(Directive::STYLE, Keyword::UNSAFE_EVAL)
-			->addDirective(Directive::SCRIPT, "http://localhost:8080")
-			->addDirective(Directive::SCRIPT, Keyword::UNSAFE_EVAL)
-			->addDirective(Directive::SCRIPT, Keyword::UNSAFE_INLINE)
-			->addDirective(Directive::CONNECT, "ws://localhost:8080")
-			->addDirective(Directive::CONNECT, "wss://192.168.1.35:6001")
-			->addDirective(Directive::CONNECT, 'wss://*.pusher.com')
-			->addDirective(Directive::SCRIPT, 'wss://*.pusher.com');
+			// Custom
+			->addDirective(Directive::DEFAULT, [
+				"ws: wss:",
+				"localhost:8080",
+				"https://fonts.gstatic.com https://fonts.googleapis.com",
+				"*.pusher.com",
+			])
+			->addDirective(Directive::IMG, ["*.openstreetmap.fr"])
+			->addDirective(Directive::CONNECT, ["ws: wss: *.pusher.com"])
+			->addDirective(Directive::STYLE, [
+				Keyword::UNSAFE_EVAL,
+				Keyword::UNSAFE_INLINE,
+				"https://fonts.gstatic.com https://fonts.googleapis.com",
+			])
+			->addDirective(Directive::SCRIPT, [
+				Keyword::UNSAFE_EVAL,
+				Keyword::UNSAFE_INLINE,
+			]);
 	}
 }
