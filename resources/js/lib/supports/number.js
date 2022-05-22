588f4a26d92ed85d981b20ec0404303d8f87ff56
Élie Meignan <contact@eliepse.fr>
2022-05-22 22:36:35 +0200

    Add helpers about speed

--- resources/js/lib/supports/number.js
+++ resources/js/lib/supports/number.js
@@ -7,3 +7,23 @@ export function geoDistance([lat1, lon1], [lat2, lon2]) {
 		(Math.cos(lat1 * p) * Math.cos(lat2 * p) * (1 - Math.cos((lon2 - lon1) * p))) / 2;
 	return 12742000 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
+
+/**
+ * Calculate speed from distance and duration.
+ * @param {Number} distance - in meters
+ * @param {Number} duration - in seconds
+ * @returns {Number} - speed in m/s
+ */
+export function calcSpeed(distance, duration) {
+	return distance / duration;
+}
+
+/**
+ * Convert m/s to km/h.
+ *
+ * @param {Number} speed - in m/s
+ * @returns {Number} - speed in km/s
+ */
+export function msToKmh(speed) {
+	return speed * 3.6;
+}
